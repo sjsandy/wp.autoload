123bc7dbbe54ab864765bbe7a947a9d112e7e743
Shawn Sandy <shawnsandy04@gmail.com>
2014-08-19 16:54:31 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP.Autoload V.3.0
+Plugin URI: http://autoloadmanager.shawnsandy.com
+Description: Easily extend the power of WordPress using PHP classes and libraries; reduce code repetition and easily add external packages to your WordPress projects!
+Author URI: http://shawnsandy.com
+Version: 2.0 Beta
+ */
+
+?>
