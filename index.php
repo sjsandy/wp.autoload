80f2066ff4d81f90ff8f2e430153b6341ab30156
shawnsandy <shawnsandy04@gmail.com>
2014-08-27 16:01:25 -0400

    Composer fix

--- index.php
+++ index.php
@@ -8,4 +8,24 @@ Author URI: http://shawnsandy.com
 Version: 2.0 Beta
  */
 
-?>
+include_once __DIR__ .'vendor/symphony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->register();
+
+add_action('plugins_loaded', 'load_composer');
+
+function load_composer() {
+	//indlude_once(plugins_url('vendor/autoload.php', __FILE__));
+	if (!empty($posts)):
+	var_dump($posts);
+	endif;
+}
+
+add_action('init', 'wpa_init');
+
+function wpa_init() {
+
+}
