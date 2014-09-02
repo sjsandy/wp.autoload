2e8e5f516a2c63069bd32a6388833c250e8e20df
shawn sandy <shawnsandy04@gmail.com>
2014-09-02 07:19:28 -0400

    updated plugin info

--- index.php
+++ index.php
@@ -3,29 +3,52 @@
 /*
 Plugin Name: MP.Autoload
 Plugin URI: http://autoloadmanager.shawnsandy.com
-Description: Use composer in wordpress to manage / autoload php libraies --- copy and paste the following line //** Composer Autoloader // `require_once (ABSPATH.'wp-content/plugins/mp-autoload/vendor/autoload.php')` above Sets up WordPress vars and included files.
+Description: Use composer in wordpress to manage / autoload php libraies --- copy and paste the following line 
+//** Composer Autoloader // `require_once (ABSPATH.'wp-content/plugins/mp-autoload/vendor/autoload.php')` above Sets up WordPress vars and included files.
 Author URI: http://shawnsandy.com
 Version: 3.0 Beta
  */
 
-include_once __DIR__ .'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$loader = new UniversalClassLoader();
-$loader->register();
 
-add_action('plugins_loaded', 'load_composer');
 
-function load_composer() {
-	//indlude_once(plugins_url('vendor/autoload.php', __FILE__));
-	if (!empty($posts)):
-	var_dump($posts);
-	endif;
-}
+/**
+* wordpress plugin 
+*/
 
-add_action('init', 'wpa_init');
+class mp_autoload 
+{
+	
+	function __construct()
+	{
+		# code...
+		$this->actions();
+	}
 
-function wpa_init() {
+	/**
+	 * Add all plugin actions here will be called by class construct
+	 */
+	public function actions() {
+
+	add_action('plugins_loaded', array($this, 'plugins_loaded'));
+
+	add_action('init', array($this, 'init'));
+
+	add_action("admin_init", array($this, "admin_init"));
+
+	}
+
+	public function plugins_loaded() {
+
+	}
+
+	public function init() {
+
+	}
+
+	public function admin_init() {
+
+	}
 
 }
