e4688ea8060075698789db10fdd0d65b75a1fcb1
shawnsandy <shawnsandy04@gmail.com>
2014-09-04 14:41:05 -0400

    Updated read me Added plugin page -- index.php

--- index.php
+++ index.php
@@ -3,26 +3,32 @@
 /*
 Plugin Name: MP.Autoload
 Plugin URI: http://autoloadmanager.shawnsandy.com
-Description: Use composer in wordpress to manage / autoload php libraies --- copy and paste the following line 
-//** Composer Autoloader // `require_once (ABSPATH.'wp-content/plugins/mp-autoload/vendor/autoload.php')` above Sets up WordPress vars and included files.
+Description: Use composer in wordpress to manage / autoload php libraies --- copy and paste the following line
+//** Composer Autoloader // `require_once (ABSPATH.'wp-content/plugins/mp-autoload/vendor/autoload.php')` above Sets up WordPress vars and included files in your wp-config.php.
 Author URI: http://shawnsandy.com
 Version: 3.0 Beta
  */
 
-
-
-
-
 /**
-* wordpress plugin 
-*/
+ * wordpress plugin
+ */
 
-class mp_autoload 
-{
-	
-	function __construct()
-	{
-		# code...
+class mp_autoload {
+
+	private static $instance;
+
+	/**
+	 *
+	 * @return type
+	 */
+	public static function instance() {
+		if (!isset(self::$instance)) {
+			self::$instance = new mp_autoload();
+		}
+		return self::$instance;
+	}
+
+	function __construct() {
 		$this->actions();
 	}
 
@@ -31,11 +37,13 @@ class mp_autoload
 	 */
 	public function actions() {
 
-	add_action('plugins_loaded', array($this, 'plugins_loaded'));
+		add_action('plugins_loaded', array($this, 'plugins_loaded'));
 
-	add_action('init', array($this, 'init'));
+		add_action('init', array($this, 'init'));
 
-	add_action("admin_init", array($this, "admin_init"));
+		add_action("admin_init", array($this, "admin_init"));
+
+		add_action('admin_menu', array($this, 'menus'));
 
 	}
 
@@ -51,4 +59,18 @@ class mp_autoload
 
 	}
 
+	public function menus() {
+
+		add_menu_page('MP.Autoload', "MP.Autoload", 'manage_options', __FILE__, array($this, 'admin_page'));
+
+	}
+
+	public function admin_page() {
+
+		include_once (__DIR__ ."/views/index.php");
+
+	}
+
 }
+
+mp_autoload::instance();
